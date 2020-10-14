072f5370a8200712ae363c6635fd7f5d893c77ac
Florian Weiss <if20b216@technikum-wien.at>
2020-10-14 16:48:10 +0200

    Add project files.

---
+++ SWE1-MTCG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SWE1_MTCG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
