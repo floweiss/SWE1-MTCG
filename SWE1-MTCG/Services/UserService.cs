cd945993b0841b60fe0f17baf39199a45627b3bd
Florian Weiss <floweiss6@gmail.com>
2020-12-13 20:03:38 +0100

    Refactor UserService

--- SWE1-MTCG/Services/UserService.cs
+++ SWE1-MTCG/Services/UserService.cs
@@ -12,7 +12,9 @@ namespace SWE1_MTCG.Services
 {
     public class UserService : IUserService
     {
+        private IUserDataService _userDataService = new UserDataService();
         private string _cs = "Host=localhost;Username=postgres;Password=postgres123;Database=postgres";
+
         public string Register(User user)
         {
             // http://zetcode.com/csharp/postgresql/
@@ -70,7 +72,7 @@ namespace SWE1_MTCG.Services
                     {
                         userToken = user.Username + "-mtcgToken";
                         ClientSingleton.GetInstance.ClientMap.AddOrUpdate(userToken, user, (key, oldValue) => user);
-                        LoadUserData(user, userToken);
+                        _userDataService.LoadUserData(user, userToken);
                         return "POST OK - Authentication-Token: "+userToken;
                     }
                 }
@@ -161,113 +163,8 @@ namespace SWE1_MTCG.Services
             }
             user.AddCardsToStack(pack);
             readerCards.Close();
-            PersistUserData(user, usertoken);
+            _userDataService.PersistUserData(user, usertoken);
             return "POST OK";
-        }
-
-        private void PersistUserData(User user, string usertoken)
-        {
-            using NpgsqlConnection con = new NpgsqlConnection(_cs);
-            con.Open();
-
-            using NpgsqlCommand cmd = new NpgsqlCommand();
-            cmd.Connection = con;
-            cmd.CommandText = @"CREATE TABLE IF NOT EXISTS userdata(token VARCHAR(255), coins INTEGER, deck VARCHAR(255), stack VARCHAR(800))";
-            cmd.ExecuteNonQuery();
-
-            string sqlCheckUser = "SELECT * FROM userdata";
-            using NpgsqlCommand cmdCheckUser = new NpgsqlCommand(sqlCheckUser, con);
-            using NpgsqlDataReader reader = cmdCheckUser.ExecuteReader();
-            bool updateUser = false;
-            while (reader.Read())
-            {
-                if (reader.GetString(0) == usertoken)
-                {
-                    updateUser = true;
-                }
-            }
-            reader.Close();
-            if (updateUser)
-            {
-                string sqlUpdate =
-                    "UPDATE userdata SET token = @token, coins = @coins, deck = @deck, stack = @stack WHERE token = @findToken";
-                using (NpgsqlCommand cmdPrepared = new NpgsqlCommand(sqlUpdate, con))
-                {
-                    cmdPrepared.Parameters.AddWithValue("token", usertoken);
-                    cmdPrepared.Parameters.AddWithValue("findToken", usertoken);
-                    cmdPrepared.Parameters.AddWithValue("coins", user.Coins);
-                    cmdPrepared.Parameters.AddWithValue("deck", "{ \"CardIds\":" + JsonSerializer.Serialize(user.Deck.ToCardIds()) + "}");
-                    cmdPrepared.Parameters.AddWithValue("stack", "{ \"CardIds\":" + JsonSerializer.Serialize(user.Stack.ToCardIds()) + "}");
-                    cmdPrepared.ExecuteNonQuery();
-                }
-            }
-            else
-            {
-                var sqlInsert = "INSERT INTO userdata (token, coins, deck, stack) VALUES (@token, @coins, @deck, @stack)";
-                using (NpgsqlCommand cmdPrepared = new NpgsqlCommand(sqlInsert, con))
-                {
-                    cmdPrepared.Parameters.AddWithValue("token", usertoken);
-                    cmdPrepared.Parameters.AddWithValue("coins", user.Coins);
-                    cmdPrepared.Parameters.AddWithValue("deck", "{ \"CardIds\":" + JsonSerializer.Serialize(user.Deck.ToCardIds()) + "}");
-                    cmdPrepared.Parameters.AddWithValue("stack", "{ \"CardIds\":" + JsonSerializer.Serialize(user.Stack.ToCardIds()) + "}");
-                    cmdPrepared.ExecuteNonQuery();
-                }
-            }
-        }
-
-        private void LoadUserData(User user, string usertoken)
-        {
-            using NpgsqlConnection con = new NpgsqlConnection(_cs);
-            con.Open();
-
-            using NpgsqlCommand cmd = new NpgsqlCommand();
-            cmd.Connection = con;
-            cmd.CommandText = @"CREATE TABLE IF NOT EXISTS userdata(token VARCHAR(255), coins INTEGER, deck VARCHAR(255), stack VARCHAR(800))";
-            cmd.ExecuteNonQuery();
-
-            string sqlCheckUser = "SELECT * FROM userdata";
-            using NpgsqlCommand cmdCheckUser = new NpgsqlCommand(sqlCheckUser, con);
-            using NpgsqlDataReader reader = cmdCheckUser.ExecuteReader();
-            List<string> deckIds = new List<string>();
-            List<string> stackIds = new List<string>();
-            while (reader.Read())
-            {
-                if (reader.GetString(0) == usertoken)
-                {
-                    user.Coins = reader.GetInt32(1);
-                    deckIds = JsonSerializer.Deserialize<CardIdsDTO>(reader.GetString(2)).CardIds;
-                    stackIds = JsonSerializer.Deserialize<CardIdsDTO>(reader.GetString(3)).CardIds;
-                }
-            }
-            reader.Close();
-
-            string sqlAllCards = "SELECT * FROM cards";
-            using NpgsqlCommand cmdCards = new NpgsqlCommand(sqlAllCards, con);
-            using NpgsqlDataReader readerCards = cmdCards.ExecuteReader();
-            while (readerCards.Read())
-            {
-                foreach (var cardId in deckIds)
-                {
-                    if (readerCards.GetString(0) == cardId)
-                    {
-                        CardDTO cardDto = new CardDTO(readerCards.GetString(0), readerCards.GetString(1), readerCards.GetString(2), readerCards.GetString(3), readerCards.GetDouble(4));
-                        Card card = cardDto.ToCard();
-                        user.Deck.AddCard(card);
-                    }
-                }
-
-                foreach (var cardId in stackIds)
-                {
-                    if (readerCards.GetString(0) == cardId)
-                    {
-                        CardDTO cardDto = new CardDTO(readerCards.GetString(0), readerCards.GetString(1), readerCards.GetString(2), readerCards.GetString(3), readerCards.GetDouble(4));
-                        Card card = cardDto.ToCard();
-                        user.Stack.AddCard(card);
-                    }
-                }
-            }
-
-            ClientSingleton.GetInstance.ClientMap.AddOrUpdate(usertoken, user, (key, oldValue) => user);
         }
     }
 }
